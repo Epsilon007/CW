8bad80fecc49fca80da55714a8075c7e503e5630
JiangJL <987102561@qq.com>
2020-12-15 20:41:04 +0800

    1_Python基础

---
+++ Python-100-Days-案例分析.py
@@ -0,0 +1,104 @@
+# 2020年12月10日14:57:55
+
+### day4
+# 打印三角形图案
+print("\n day4_打印三角形图案 \n".center(50, '='))
+
+row = int(input('请输入行数: '))
+for i in range(row):
+    for _ in range(i + 1):
+        print('*', end='')
+    print()
+
+
+for i in range(row):
+    for j in range(row):
+        if j < row - i - 1:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print()
+
+for i in range(row):
+    for _ in range(row - i - 1):
+        print(' ', end='')
+    for _ in range(2 * i + 1):
+        print('*', end='')
+    print()
+
+
+### day5
+# 正整数反转
+print("\n day5_正整数反转 \n".rjust(50, '='))
+
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
+
+
+### day7
+# 设计一个函数返回给定文件名的后缀名
+print("\n day7_设计一个函数返回给定文件名的后缀名 \n".rjust(50, '='))
+
+def get_suffix(filename, has_dot=False):
+    """
+    获取文件名的后缀名
+
+    :param filename: 文件名
+    :param has_dot: 返回的后缀名是否需要带点
+    :return: 文件的后缀名
+    """
+    pos = filename.rfind('.')
+    if 0 < pos < len(filename) - 1:
+        index = pos if has_dot else pos + 1
+        return filename[index:]
+    else:
+        return ''
+
+print(get_suffix("pinyin.textgrid"))
+
+# 计算指定的年月日是这一年的第几天
+print("\n day7_计算指定的年月日是这一年的第几天 \n".rjust(50, '='))
+
+def is_leap_year(year):
+    """
+    判断指定的年份是不是闰年
+
+    :param year: 年份
+    :return: 闰年返回True平年返回False
+    """
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def which_day(year, month, date):
+    """
+    计算传入的日期是这一年的第几天
+
+    :param year: 年
+    :param month: 月
+    :param date: 日
+    :return: 第几天
+    """
+    days_of_month = [
+        [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+        [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ][is_leap_year(year)]
+    total = 0
+    for index in range(month - 1):
+        total += days_of_month[index]
+    return total + date
+
+
+def main():
+    print(which_day(1980, 11, 28))
+    print(which_day(1981, 12, 31))
+    print(which_day(2018, 1, 1))
+    print(which_day(2016, 3, 1))
+
+
+if __name__ == '__main__':
+    main()
+
